64a6683f6ceff934a071e7950a1599bc9cd5c7c6
asanko <asanko@braintch.com>
2016-12-15 19:18:36 +0300

    Basic properties

--- build.py
+++ build.py
@@ -1,17 +1,45 @@
-from pybuilder.core import use_plugin, init
+from pybuilder.core import Author, init, use_plugin
 
 use_plugin("python.core")
-use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
-use_plugin("python.coverage")
+# use_plugin("python.coverage")
 use_plugin("python.distutils")
 
 
 name = "pybuilder-pytest"
-default_task = "publish"
+version = '0.1.0'
+authors = [Author('Alexey Sanko', 'alexeycount@gmail.com')]
+url = 'https://github.com/AlexeySanko/pybuilder_pytest'
+description = 'Please visit {0} for more information!'.format(url)
+license = 'Apache License, Version 2.0'
+summary = 'PyBuilder Pytest Plugin'
 
+default_task = ['clean', 'analyze', 'publish']
 
 @init
 def set_properties(project):
-    pass
+    # dependencies
+    project.depends_on('pytest')
+
+    # coverage
+    # project.set_property('coverage_break_build', False)
+
+    # flake8
+    project.set_property('flake8_verbose_output', True)
+    project.set_property('flake8_break_build', True)
+
+    # distutils
+    project.set_property('distutils_classifiers', [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'])
