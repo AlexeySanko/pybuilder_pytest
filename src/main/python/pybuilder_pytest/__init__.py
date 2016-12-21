00ee22c2b2adb443c99d4eb15fcf75ba34ff160a
asanko <asanko@braintch.com>
2016-12-21 21:55:08 +0300

    property "pytest_extra_args" was added

--- src/main/python/pybuilder_pytest/__init__.py
+++ src/main/python/pybuilder_pytest/__init__.py
@@ -14,6 +14,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import pytest
 from sys import path as sys_path
 
 from pybuilder.core import task, init, use_plugin
@@ -29,21 +30,20 @@ def initialize_pytest_plugin(project):
     """ Init default plugin project properties. """
     project.plugin_depends_on('pytest')
     project.set_property_if_unset("dir_source_pytest_python", "src/unittest/python")
+    project.set_property_if_unset("pytest_extra_args", [])
 
 
 @task
 def run_unit_tests(project, logger):
     """ Call pytest for the sources of the given project. """
-    args = None
-    import pytest
-
     logger.info('pytest: Run unittests.')
     from pybuilder.plugins.python.unittest_plugin \
         import _register_test_and_source_path_and_return_test_dir \
         as push_test_path_to_syspath
     test_dir = push_test_path_to_syspath(project, sys_path, 'pytest')
+    extra_args = project.get_property("pytest_extra_args")
     try:
-        pytest_args = [test_dir] + args if args else [test_dir]
+        pytest_args = [test_dir] + (extra_args if extra_args else [])
         if project.get_property('verbose'):
             pytest_args.append('-s')
             pytest_args.append('-v')
