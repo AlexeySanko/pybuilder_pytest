384a4613216fac1276f2bb8924d94e9d2e09d4f1
Martin Grůber <martin.gruber@email.cz>
2023-07-13 08:41:14 +0200

    Remove deprecated plugin dependency

--- build.py
+++ build.py
@@ -21,7 +21,7 @@ from pybuilder.core import Author, init, use_plugin, task
 use_plugin("python.core")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
-use_plugin("python.frosted")
+# use_plugin("python.frosted")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 use_plugin("python.unittest")
@@ -63,9 +63,9 @@ def set_properties(project):
     project.set_property('flake8_verbose_output', True)
     project.set_property('flake8_break_build', True)
 
-    # frosted
-    project.set_property("frosted_break_build", True)
-    project.set_property("frosted_include_test_sources", True)
+    # # frosted
+    # project.set_property("frosted_break_build", True)
+    # project.set_property("frosted_include_test_sources", True)
 
     # distutils
     project.set_property('distutils_commands', ['bdist_wheel'])
